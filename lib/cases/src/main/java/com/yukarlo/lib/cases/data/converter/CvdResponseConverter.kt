ab481529729b7b889b9aa161b6ee86a6e6614809
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2020-04-19 02:24:00 +0200

    add decimals and convert time

--- lib/cases/src/main/java/com/yukarlo/lib/cases/data/converter/CvdResponseConverter.kt
+++ lib/cases/src/main/java/com/yukarlo/lib/cases/data/converter/CvdResponseConverter.kt
@@ -1,16 +1,20 @@
 package com.yukarlo.lib.cases.data.converter
 
+import android.text.format.DateUtils
 import com.yukarlo.core.domain.model.CasesSummaryModel
 import com.yukarlo.lib.cases.data.model.CasesSummaryResponseModel
 import com.yukarlo.stack.network.model.DataToDomainConverter
+import java.text.NumberFormat
 
-internal object CvdResponseConverter : DataToDomainConverter<CasesSummaryResponseModel, CasesSummaryModel> {
+internal object CvdResponseConverter :
+    DataToDomainConverter<CasesSummaryResponseModel, CasesSummaryModel> {
     override fun convert(input: CasesSummaryResponseModel): CasesSummaryModel {
+        val numberFormat = NumberFormat.getNumberInstance()
         return CasesSummaryModel(
-            totalCasesCount = input.cases.toString(),
-            totalDeceasedCount = input.deaths.toString(),
-            totalRecoveredCount = input.recovered.toString(),
-            updatedSince = input.updated.toString()
+            totalCasesCount = numberFormat.format(input.cases),
+            totalDeceasedCount = numberFormat.format(input.deaths),
+            totalRecoveredCount = numberFormat.format(input.recovered),
+            updatedSince = DateUtils.getRelativeTimeSpanString(input.updated).toString()
         )
     }
 }
