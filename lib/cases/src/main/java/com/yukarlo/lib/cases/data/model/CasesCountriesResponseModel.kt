54d96f2e672afe1d9b386158f68d209eadc37454
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2020-04-20 22:07:50 +0200

    change type

--- lib/cases/src/main/java/com/yukarlo/lib/cases/data/model/CasesCountriesResponseModel.kt
+++
@@ -1,8 +0,0 @@
-package com.yukarlo.lib.cases.data.model
-
-import com.squareup.moshi.Json
-
-internal data class CasesCountriesResponseModel(
-    @Json(name="countries")
-    val countries: List<CasesCountryResponseModel>
-)
--- lib/cases/src/main/java/com/yukarlo/lib/cases/data/model/CasesCountryResponseModel.kt
+++ lib/cases/src/main/java/com/yukarlo/lib/cases/data/model/CasesCountryResponseModel.kt
@@ -1,10 +1,11 @@
 package com.yukarlo.lib.cases.data.model
 
 import com.squareup.moshi.Json
+import com.yukarlo.stack.network.model.DataModel
 
 internal data class CasesCountryResponseModel(
     @Json(name = "updated")
-    val updated: Int,
+    val updated: Long,
 
     @Json(name = "country")
     val country: String,
@@ -13,26 +14,26 @@ internal data class CasesCountryResponseModel(
     val countryInfo: CountryInfo,
 
     @Json(name = "cases")
-    val cases: Int,
+    val cases: Long,
 
     @Json(name = "todayCases")
-    val casesToday: Int,
+    val casesToday: Long,
 
     @Json(name = "deaths")
-    val deaths: Int,
+    val deaths: Long,
 
     @Json(name = "todayDeaths")
-    val deathsToday: Int,
+    val deathsToday: Long,
 
     @Json(name = "recovered")
-    val recovered: Int,
+    val recovered: Long,
 
     @Json(name = "active")
-    val active: Int,
+    val active: Long,
 
     @Json(name = "critical")
-    val critical: Int,
+    val critical: Long,
 
     @Json(name = "tests")
-    val tests: Int
-)
+    val tests: Long
+) : DataModel
