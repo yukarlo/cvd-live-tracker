ec8558ec6a42cc6b2dbb36f98c937cdfb99dd24d
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2020-05-14 19:04:10 +0200

    add cases country search adapter

---
+++ ui/countries/src/main/java/com/yukarlo/ui/countries/adapter/CasesCountrySearchAdapter.kt
@@ -0,0 +1,37 @@
+package com.yukarlo.ui.countries.adapter
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.core.widget.doAfterTextChanged
+import androidx.recyclerview.widget.RecyclerView
+import com.yukarlo.ui.countries.adapter.CasesCountrySearchAdapter.CasesCountrySearchViewHolder
+import com.yukarlo.ui.countries.databinding.SearchRowBinding
+
+class CasesCountrySearchAdapter(val filterCountry: (String) -> Unit) :
+    RecyclerView.Adapter<CasesCountrySearchViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CasesCountrySearchViewHolder {
+        val itemBinding =
+            SearchRowBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CasesCountrySearchViewHolder(itemBinding)
+    }
+
+    override fun getItemCount(): Int = 1
+
+    override fun onBindViewHolder(holder: CasesCountrySearchViewHolder, position: Int) {
+        holder.bind()
+    }
+
+    inner class CasesCountrySearchViewHolder(private val itemBinding: SearchRowBinding) :
+        RecyclerView.ViewHolder(itemBinding.root) {
+
+        fun bind() {
+            itemBinding.affectedPlaceSearchTextInput.doAfterTextChanged {
+                filterCountry(it.toString())
+            }
+        }
+    }
+}
