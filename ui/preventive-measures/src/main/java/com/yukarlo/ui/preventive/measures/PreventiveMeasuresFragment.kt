feb497e23c403d85b64864a845e9e33fb0915d71
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2020-05-15 18:18:41 +0200

    add link movement

--- ui/preventive-measures/src/main/java/com/yukarlo/ui/preventive/measures/PreventiveMeasuresFragment.kt
+++ ui/preventive-measures/src/main/java/com/yukarlo/ui/preventive/measures/PreventiveMeasuresFragment.kt
@@ -1,11 +1,11 @@
 package com.yukarlo.ui.preventive.measures
 
 import android.os.Bundle
+import android.text.method.LinkMovementMethod
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import com.yukarlo.ui.preventive.measures.databinding.PreventiveMeasuresFragmentBinding
 
 class PreventiveMeasuresFragment : Fragment() {
@@ -22,10 +22,8 @@ class PreventiveMeasuresFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        setUpViews()
-    }
 
-    private fun setUpViews() {
-
+        fragmentBinding.preventiveMeasuresMoreInformationTextView.movementMethod =
+            LinkMovementMethod.getInstance()
     }
 }
