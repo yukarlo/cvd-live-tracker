5382d8a460d98ad844f9b111608c604318377450
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2020-07-09 18:52:35 +0200

    Bump target version to Android 11 Bump version code to 3 Bump version name to 0.90

--- buildSrc/src/main/java/AndroidConfig.kt
+++ buildSrc/src/main/java/AndroidConfig.kt
@@ -1,11 +1,11 @@
 internal object AndroidConfig {
-    const val COMPILE_SDK_VERSION = 29
-    const val BUILD_TOOLS_VERSION = "29.0.3"
+    const val COMPILE_SDK_VERSION = 30
+    const val BUILD_TOOLS_VERSION = "30.0.1"
     const val MIN_SDK_VERSION = 23
-    const val TARGET_SDK_VERSION = 29
+    const val TARGET_SDK_VERSION = 30
 
-    const val VERSION_CODE = 2
-    const val VERSION_NAME = "0.80"
+    const val VERSION_CODE = 3
+    const val VERSION_NAME = "0.90"
 
     const val APP_ID = "com.yukarlo.coronow"
     const val TEST_INSTRUMENTATION_RUNNER = "android.support.test.runner.AndroidJUnitRunner"
