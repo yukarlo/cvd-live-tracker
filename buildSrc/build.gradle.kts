0cc8d164e8ceed534eb46e820faca474a7fe4362
Karlo Miguel P. Yu <karlo.miguel.p.yu@gmail.com>
2021-02-19 11:12:30 +0100

    update AGP

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -22,6 +22,6 @@ repositories {
 }
 
 dependencies {
-    implementation("com.android.tools.build:gradle:7.0.0-alpha05")
+    implementation("com.android.tools.build:gradle:7.0.0-alpha07")
     implementation("org.jetbrains.kotlin:kotlin-gradle-plugin:1.4.30")
 }
